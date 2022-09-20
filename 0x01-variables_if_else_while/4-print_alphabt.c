e4f43dcc8f8e772c830b8b2d6a0362d16724f12a
abrhamtafere <88852639+abrhamtafere@users.noreply.github.com>
2022-09-20 04:06:07 +0300

    Add files via upload
    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 113 || n == 101)
+		{
+		}
+		else
+		{
+			putchar(n);
+		}
+		n++;
+
+	}
+	putchar('\n');
+	return (0);
+}
